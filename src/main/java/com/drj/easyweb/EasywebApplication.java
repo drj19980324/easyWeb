6bfe641bf2addb4b2feeb72ab8d0ec999f1d68c8
dairunji <dairj199803@163.com>
2021-03-08 17:25:24 +0800

    add all files

---
+++ src/main/java/com/drj/easyweb/EasywebApplication.java
@@ -0,0 +1,13 @@
+package com.drj.easyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasywebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasywebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/drj/easyweb/EasywebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drj.easyweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasywebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
